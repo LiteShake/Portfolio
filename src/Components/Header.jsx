e8eece5248a9c917c457148f1d60d12fb5d53e05
Viswanathan <87363067+LiteShake@users.noreply.github.com>
2024-11-26 21:44:29 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className=" p-4 sticky top-0 z-50 relative bg-slate-800/60 backdrop-blur-xl">
       <ul className='flex flex-row place-content-evenly'>
         <li>
-          <a href='../public/resume.pdf' className='p-2 m-2 font-bold text-white bg-sky-500 rounded-full'>Resume</a>
+          <a href='public/resume.pdf' download="Viswa_Resume.pdf" className='p-2 m-2 font-bold text-white bg-sky-500 rounded-full'>Resume</a>
         </li>
         <li>
           <a href='' className='p-2 m-2 font-bold text-white bg-sky-500 rounded-full'>Viswanathan</a>
