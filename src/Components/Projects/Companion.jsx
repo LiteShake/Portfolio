b3781319d44773808c178d6985da4d1a68c813d8
Viswanathan <87363067+LiteShake@users.noreply.github.com>
2024-11-26 16:51:03 +0530

    added new projects

--- src/Components/Projects/Companion.jsx
+++ src/Components/Projects/Companion.jsx
@@ -14,13 +14,13 @@ const Companion = () => {
             <div className='py-4 md:py-2 text-white font-semibold text-xl px-2 md:px-2 m-1 place-content-center'>
                 <ul>
                     <li className='transition duration-500 ease-in-out backdrop-blur-md hover:shadow-md hover:shadow-purple-500/50 bg-purple-800/60 p-3 my-2 hover:text-white hover:backdrop-blur-xl rounded-xl'>
-                    A language model based on the pretty popular transformer decoder architecture pretrained and finetuned in freshly extracted discord and reddit conversations for casual conversations.
+                    A virtual assistant / remote control for my laptop which can be controlled using python running in a smartphone or via Discord.
                     </li>
                     <li className='transition duration-500 ease-in-out backdrop-blur-md hover:shadow-md hover:shadow-purple-500/50 bg-purple-800/60 p-3 my-2 hover:text-white hover:backdrop-blur-xl rounded-xl'>
-                    The main task of this model is to do question answering but we chain the questions with previous data (also known as context) and give it to the model essentially making it engage in whole conversations.
+                    A server side application that uses AI to achieve ease-of-use for my computer along with a visual terminal application to control it.
                     </li>
                     <li className='transition duration-500 ease-in-out backdrop-blur-md hover:shadow-md hover:shadow-purple-500/50 bg-purple-800/60 p-3 my-2 hover:text-white hover:backdrop-blur-xl rounded-xl'>
-                    This language model works in the representation space and is quantized for maximum efficiency in very less time.
+                    Uses Gemma and a Custom-built language model(to get a unique experience) to understand language and talk like a friendly virtual assistant and help with repetetive tasks in the system.
                     </li>
                 </ul>
             </div>
--- src/Components/Projects/StellaLM.jsx
+++ src/Components/Projects/StellaLM.jsx
@@ -15,7 +15,7 @@ const StellaLM = () => {
                 <div className='py-4 md:py-2 text-white font-semibold text-xl px-2 md:px-2 m-1 place-content-center'>
                     <ul>
                         <li className='transition duration-500 ease-in-out backdrop-blur-md hover:shadow-md hover:shadow-sky-500/50 bg-sky-800/60 p-3 my-2 hover:text-white hover:backdrop-blur-xl rounded-xl'>
-                            A language model based on the pretty popular transformer decoder architecture pretrained and finetuned in freshly extracted discord and reddit conversations for casual conversations.
+                            A language model based on the pretty popular transformer decoder architecture pretrained and finetuned in fresh extracted discord and reddit conversations for casual conversations.
                         </li>
                         <li className='transition duration-500 ease-in-out backdrop-blur-md hover:shadow-md hover:shadow-sky-500/50 bg-sky-800/60 p-3 my-2 hover:text-white hover:backdrop-blur-xl rounded-xl'>
                             The main task of this model is to do question answering but we chain the questions with previous data (also known as context) and give it to the model essentially making it engage in whole conversations.
